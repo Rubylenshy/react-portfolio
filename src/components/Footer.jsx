803e9ecc3bc45ef3e37ed079728d3c7dfd5d7a5f
Rubylenshy <reztomoloju@gmail.com>
2026-02-26 17:05:17 +0100

    refactor: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,10 @@ import { Monitor, Sun, Moon } from "lucide-react";
 
 const Footer = () => {
     return (
-        <footer className="py-6 px-8 border-t border-white/5 text-center md:text-left mb-12 md:mb-0">
-            <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-4 md:gap-0">
-                <p className="font-mono text-[10px] text-gray-600 uppercase tracking-widest">
-                    © MMXXVI -  Reuben Oluwafemi. Engineered with Precision.
-                </p>
+        <footer className="py-6 px-8 flex justify-center items-center border-t border-white/5 md:text-left mb-12 md:mb-0">
+            <p className="font-mono text-[10px] text-gray-600 uppercase tracking-widest">© MMXXVI -  Reuben Oluwafemi. Engineered with Precision.</p>
+            {/* <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-4 md:gap-0">
+                
 
                 <div className="flex items-center gap-1 bg-gray-100/10 rounded-full p-1">
                     <button
@@ -28,7 +27,7 @@ const Footer = () => {
                         <Moon className="w-4 h-4 text-gray-400" />
                     </button>
                 </div>
-            </div>
+            </div> */}
         </footer>
     );
 };
